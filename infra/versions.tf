9b6c4177161794a2fbb3e611d269ffb0c914fd8c
Chris Van Law <chris@chrisvanlaw.com>
2023-11-25 10:44:48 -0500

    upgrade to 5.x version of aws provider

--- infra/versions.tf
+++ infra/versions.tf
@@ -2,9 +2,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.24"
+      version = "~> 5.26"
     }
   }
 
-  required_version = ">= 1.4.0"
+  required_version = ">= 1.5.0"
 }
